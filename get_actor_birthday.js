7c72dec18b99bdedb3d85b696a1f09beaa135c68
Samir Madhavan <samir.madhavan@zapprx.com>
2017-05-16 18:15:54 +0530

    Improved the code by performing multiple async requests for the birthday and batching it

--- get_actor_birthday.js
+++ get_actor_birthday.js
@@ -2,21 +2,24 @@ var fs = require('fs');
 var util = require('util');
 var imdb = require('./index');
 var underscore = require('underscore');
+var async = require('async');
 
 function init() {
     var data = JSON.parse(fs.readFileSync('./output/actor_links.json', 'utf8')),
         output = [];
 
     console.log("The number of actors in the file: " + data.length);
-    getActorBirthday(data, output, 9998, data.length);
+    getActorBirthday(data.splice(0, 200), output);
 }
 
-function getActorBirthday(data, result, index, endingIndex) {
-    if (index === endingIndex) {
-    	console.log("Writing the birthday data to the file");
 
+function getActorBirthday(data, output, cb, index, endingIndex) {
+    var d = data.splice(0, 100);
+
+    if (d.length === 0) {
+    	console.log("Writing the birthday data data to the file");
         file = fs.createWriteStream('./output/actor_birthday_data.csv');
-        result.forEach(function(e) {
+        output.forEach(function(e) {
             var values = underscore.values(e);
 
             values = values.join(",");
@@ -27,20 +30,29 @@ function getActorBirthday(data, result, index, endingIndex) {
 
         file.end();
     } else {
-    	console.log("Fetching data for actor: " + data[index].name + " ranked: " + data[index].rank);
-        imdb(data[index].link, function(err, d) {
+        async.map(d, function(dataObject, callback) {
+
+            imdb(dataObject.link, function(err, d) {
+                if (err)
+                    console.log(err.stack);
+
+                if (d)
+                    callback(null, {
+                        name: dataObject.name,
+                        rank: dataObject.rank,
+                        gender: dataObject.gender,
+                        birthday: d.birthday
+                    });
+            });
+
+        }, function(err, result) {
             if (err)
                 console.log(err.stack);
 
-            if (d)
-                result.push({
-                    name: data[index].name,
-                    rank: data[index].rank,
-                    gender: data[index].gender,
-                    birthday: d.birthday
-                });
-            index = index + 1
-            getActorBirthday(data, result, index, endingIndex)
+            if (result) {
+                output = output.concat(result);
+                getActorBirthday(data, output);
+            }
         });
     }
 }
